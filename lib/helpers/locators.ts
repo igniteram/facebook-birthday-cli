6b68b886819469f23bce43f8389781acabd8b978
Ram Pasala <ram.pasala7@gmail.com>
2018-08-19 13:47:53 +0530

    chore(locators): removed unwanted locators & added clang formatting
    * removed unwanted locators
    * added clang format file
    * added git attributes for line endings
    * incremented version to 0.2.0

--- lib/helpers/locators.ts
+++ lib/helpers/locators.ts
@@ -6,13 +6,8 @@ interface Locator {
   logoutLink: string;
   logoutButton: string;
   birthdayTodayCard: string;
-  birthdayDivCard: string;
-  birthdays: string;
   birthdayNames: string;
   birthdayText: string;
-  recentBirthdayCard: string;
-  recentBirthdayNames: string;
-  recentBirthText: string;
 }
 const locators: Locator = {
   url : 'https://www.facebook.com/events/birthdays/',
@@ -22,16 +17,9 @@ const locators: Locator = {
   logoutLink : 'a#pageLoginAnchor',
   logoutButton : '//span[contains(text(),"Log Out")]',
   birthdayTodayCard : 'div#birthdays_today_card',
-  birthdayDivCard : 'div._fbBirthdays__todayCard',
-  birthdays : 'div._fbBirthdays__todayCard > div:nth-child(2) >ul >li',
   birthdayNames :
       'div._fbBirthdays__todayCard > div:nth-child(2) a:not([href*="friendship"])',
-  birthdayText : 'div._fbBirthdays__todayCard > div:nth-child(2) textarea',
-  recentBirthdayCard : 'div#birthdays_recent_card',
-  recentBirthdayNames :
-      'div._fbBirthdays__recentCard > div:nth-child(2) a:not([href*="friendship"])',
-  recentBirthText : 'div._fbBirthdays__recentCard > div:nth-child(2) textarea',
-
+  birthdayText : 'div._fbBirthdays__todayCard > div:nth-child(2) textarea'
 };
 
 export {locators};
